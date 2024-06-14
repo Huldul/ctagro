1f857eca017c54fad5bdabd3b2931a11e4cf83e8
Huldul <kiriunchik@gmail.com>
2024-06-14 10:49:44 +0500

    contacts

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -3,7 +3,7 @@
 @section('content')
         <main>
             <section class="first__block">
-                <img src="./img/first-block-img.png" alt="">
+                <img src="{{asset("img/first-block-img.png")}}" alt="">
                 <div class="first__block-wrapper container">
                     <h1>Контакты</h1>
                 </div>
@@ -115,38 +115,7 @@
                     </svg>
 
             </section>
-            <section class="form about__form indent">
-                <div class="form__wrapper">
-                    <img src="./img/form-bg.png" alt="">
-                    <div class="form__content container">
-                        <h2 class="form__title title">Свяжитесь с нами для обсуждения деталей</h2>
-                        <form action="#">
-                            <div class="form__group">
-                                <input type="text" name="" id="" placeholder="Имя*" required>
-                                <input type="tel" name="" id="" placeholder="Телефон*" required>
-                                <div class="select-wrapp">
-                                    <select name="" id="" required>
-                                        <option value="" disabled selected>Регион*</option>
-                                        <option value="Astana">Астана</option>
-                                        <option value="Astana">Астана</option>
-                                    </select>
-                                </div>
-                                <input type="email" name="" id="" placeholder="Почта*" required pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$" title="Введите корректный email в формате user@example.com">
-                            </div>
-                            <div class="form__group">
-                                <div class="select-wrapp">
-                                    <select>
-                                        <option value="" disabled selected>Тема</option>
-                                        <option value="lorem">lorem</option>
-                                    </select>
-                                </div>
-                                <button type="submit">Отправить</button>
-                            </div>
-                            <span>Нажимая кнопку “Отправить” вы даёте согласие на обработку  <a href="#">персональных данных</a></span>
-                        </form>
-                    </div>
-                </div>
-            </section>
+            @include('components.form')
         </main>
         @endsection
 

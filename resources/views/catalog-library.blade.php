a68f20536d6432fc90516d2aee00d4b250e6ec26
Huldul <kiriunchik@gmail.com>
2024-06-27 15:22:46 +0500

    23

--- resources/views/catalog-library.blade.php
+++ resources/views/catalog-library.blade.php
@@ -5,7 +5,7 @@
     <section class="first__block">
         <img src="{{ asset('img/first-block-img.png') }}" alt="">
         <div class="first__block-wrapper container">
-            <h1>Библиотека каталогов</h1>
+            <h1>Каталог</h1>
         </div>
     </section>
     <div class="breadcrumbs container">
@@ -19,12 +19,7 @@
                 </svg>
             </li>
             <li>Каталог</li>
-            <li>
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.0037 8L6.00372 13L6.00372 3L11.0037 8Z" fill="#6EB513" />
-                </svg>
-            </li>
-            <li>Библиотека каталогов</li>
+
         </ul>
     </div>
     <section class="catalog-library container">

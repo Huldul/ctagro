30b7ae5838d8d7b74a80818c9c964e595f410a52
Huldul <kiriunchik@gmail.com>
2024-06-27 21:13:09 +0500

    Настройка сервис

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -24,7 +24,7 @@
                             <path d="M11.0037 8L6.00372 13L6.00372 3L11.0037 8Z" fill="#6EB513" />
                         </svg>
                     </li>
-                    <li>{!! $page->subtitle1!!}</li>
+                    <li>{!! $page->title!!}</li>
                 </ul>
             </div>
             <section class="service container">

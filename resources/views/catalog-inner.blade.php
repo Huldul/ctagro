8b6eef93d6fddf8319a148347783100c4e49a192
Huldul <kiriunchik@gmail.com>
2024-06-27 15:16:47 +0500

    23

--- resources/views/catalog-inner.blade.php
+++ resources/views/catalog-inner.blade.php
@@ -18,7 +18,7 @@
                             <path d="M11.0037 8L6.00372 13L6.00372 3L11.0037 8Z" fill="#6EB513" />
                         </svg>
                     </li>
-                    <li><a href="{{ route('catalog-library', ['locale' => app()->getLocale()]) }}">Каталог></li>
+                    <li><a href="{{ route('catalog-library', ['locale' => app()->getLocale()]) }}">Каталог</a></li>
 
                     <li>
                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">

9aa99fa7457a28c0bb98cf82ac562b1d296c4c9e
vadimprokofev14 <vadim61prokofev19971401@gmail.com>
2024-06-28 11:44:17 +0500

    Update catalog-online.blade.php

--- resources/views/catalog-online.blade.php
+++ resources/views/catalog-online.blade.php
@@ -4,7 +4,7 @@
 <section class="first__block">
         <img src="{{ asset('img/first-block-img.png') }}" alt="">
         <div class="first__block-wrapper container">
-            <h1>{{ $brand->title }}</h1>
+            <h1>Онлайн каталог</h1>
         </div>
     </section>
     <div class="_df_book" id="dflip-container" src="/catalog_ctagro.pdf"></div>

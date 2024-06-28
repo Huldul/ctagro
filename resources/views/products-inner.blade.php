d46d790ba2a03268d3c7a6fb25b61d437e572d15
Huldul <90949230+Huldul@users.noreply.github.com>
2024-06-28 18:59:36 +0500

    Update products-inner.blade.php

--- resources/views/products-inner.blade.php
+++ resources/views/products-inner.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 
 @section('content')
+@dd($product)
         <main>
             <section class="first__block">
                 <img src="{{asset("img/first-block-img.png")}}" alt="">

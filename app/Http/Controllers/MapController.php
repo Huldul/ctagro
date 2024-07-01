476305623be0be694c277003f3712db23dfd992b
Huldul <kiriunchik@gmail.com>
2024-07-01 16:32:28 +0500

    Api test

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -7,10 +7,22 @@ use Illuminate\Http\Request;
 
 class MapController extends Controller
 {
-    public function show($id, $locale){
+    public function show($id, $locale)
+    {
+        // Получение не переведенной карты по идентификатору
         $untrmap = MapKey::findOrFail($id);
+
+        // Перевод карты на текущий локаль
         $map = $untrmap->translate(app()->getLocale());
-        $json = $map->toJson();
+
+        // Преобразование переведенной карты в массив
+        $mapArray = $map->toArray();
+
+        // Преобразование массива в JSON
+        $json = json_encode($mapArray);
+
+        // Вывод JSON для отладки
         dd($json);
     }
+
 }

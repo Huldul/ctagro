018f44ba235d7831a6078ffd88434de1e4e50982
Huldul <kiriunchik@gmail.com>
2024-07-01 19:00:35 +0500

    Настройка заявок

--- resources/views/mail/order.blade.php
+++ resources/views/mail/order.blade.php
@@ -2,11 +2,13 @@
 <html>
 <head>
     <title>Новая заявка</title>
-
 </head>
 <body>
-
+    <h1>Новая заявка</h1>
+    <p><strong>Имя:</strong> {{ $data['name'] }}</p>
+    <p><strong>Номер:</strong> {{ $data['number'] }}</p>
+    <p><strong>Email:</strong> {{ $data['email'] }}</p>
+    <p><strong>Текст:</strong> {{ $data['text'] }}</p>
+    <p><strong>Регион:</strong> {{ $data['region'] }}</p>
 </body>
 </html>
-
-@endif

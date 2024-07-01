a23c32f0573f20c9a2b36a1025eef65780131680
Huldul <kiriunchik@gmail.com>
2024-07-01 16:36:51 +0500

    Api test

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -7,47 +7,20 @@ use Illuminate\Http\Request;
 
 class MapController extends Controller
 {
-    public function show($id)
-{
-    // Получение не переведенной карты по идентификатору
-    $untrmap = MapKey::findOrFail($id);
+    public function show($id, $locale)
+    {
+        // Получение не переведенной карты по идентификатору
+        $untrmap = MapKey::findOrFail($id);
 
-    // Перевод карты на текущий локаль
-    $map = $untrmap->translate(app()->getLocale());
+        // Перевод карты на текущий локаль
+        $map = $untrmap->translate(app()->getLocale());
 
-    // Преобразование атрибутов переведенного объекта в массив
-    $mapArray = [
-        'id' => $map->id,
-        'slug' => $map->slug,
-        'head_title' => $map->head_title,
-        'subtitle1' => $map->subtitle1,
-        'subtitle2' => $map->subtitle2,
-        'title1' => $map->title1,
-        'title2' => $map->title2,
-        'title3' => $map->title3,
-        'title4' => $map->title4,
-        'name1' => $map->name1,
-        'name2' => $map->name2,
-        'name3' => $map->name3,
-        'name4' => $map->name4,
-        'num1' => $map->num1,
-        'num2' => $map->num2,
-        'num3' => $map->num3,
-        'num4' => $map->num4,
-        'email1' => $map->email1,
-        'email2' => $map->email2,
-        'email3' => $map->email3,
-        'email4' => $map->email4,
-        'created_at' => $map->created_at,
-        'updated_at' => $map->updated_at,
-    ];
+        // Преобразование атрибутов переведенного объекта в массив
+        $mapArray = $map->getAttributes();
 
-    // Преобразование массива в JSON
-    $json = json_encode($mapArray);
-
-    // Вывод JSON для отладки
-    dd($json);
-}
+        // Возвращение массива в формате JSON
+        return response()->json($mapArray, 200, [], JSON_UNESCAPED_UNICODE);
+    }
 
 
 }

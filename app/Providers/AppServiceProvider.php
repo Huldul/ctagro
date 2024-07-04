035a4644d372faa7526770abeb97fa360825095f
Huldul <kiriunchik@gmail.com>
2024-07-04 14:57:26 +0500

    Изменил локализацию дат

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Carbon\Carbon;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Устанавливаем русскую локализацию для Carbon
+        Carbon::setLocale('ru');
     }
 }

f6de4a92a8a8a26b7aa284be698f4b745a930c04
vadimprokofev14 <vadim61prokofev19971401@gmail.com>
2024-07-05 13:12:13 +0500

    update search btn

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -94,7 +94,13 @@
                 </div>
                 <div class="header__search search">
                     <button class="search__btn change-color">
-                        Поиск
+                        <span>Поиск</span>
+                        <div class="search__btn-icon">
+                        <svg width="25" height="25" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" stroke="#fff"></path>
+								<path d="M17.5 17.5L13.875 13.875" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" stroke="#fff"></path>
+							</svg>
+                        </div>
                     </button>
                     <form action="#">
                         <input type="search" required>

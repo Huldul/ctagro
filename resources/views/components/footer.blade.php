aabb3cae3824e861b0152d773830254fb1290664
vadimprokofev14 <vadim61prokofev19971401@gmail.com>
2024-07-05 14:08:56 +0500

    update footer nav drop

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -126,6 +126,18 @@
                         <a href="#">Ремонт двигателей</a>
                     </li>
                 </ul>
+                <div class="navigation__list-mob">
+                    <h3>Контакты
+                         <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11 8.5L6 13.5L6 3.5L11 8.5Z" fill="#6EB513"/>
+                        </svg>
+                    </h3>
+                    <ul class="navigation__list">
+                    <li>
+                        <a href="{{ route('contacts', ['locale' => app()->getLocale()]) }}">Карта присутствия</a>
+                    </li>
+                    </ul>
+                </div>
                 <ul class="navigation__list">
                     <h3>Контакты</h3>
                     <li>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="/js/dflip/css/dflip.min.css">
     <link rel="stylesheet" href="/js/dflip/css/themify-icons.min.css">
     <link rel="stylesheet" href="{{asset("css/main.css")}}">
-    <link rel="stylesheet" href="{{asset("css/styles.css")}}?v=1.30">
+    <link rel="stylesheet" href="{{asset("css/styles.css")}}?v=1.31">
 </head>
 
 <body>

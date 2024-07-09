05450d0b31e943da99fe459a3baf8ba8bd7ac39f
Huldul <kiriunchik@gmail.com>
2024-07-09 12:02:14 +0500

    Add library db

---
+++ app/Models/LibraryPdf.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class LibraryPdf extends Model
+{
+    use HasFactory;
+    public static function boot(){
+        parent::boot();
+
+        self::creating(function($model){
+            $slug = Str::slug($model->title);
+            $model->slug = LibraryPdf::where('slug', $slug)->exists() ? $slug.'-'.uniqid() : $slug;
+        });
+
+        self::updating(function($model){
+            $slug = Str::slug($model->title);
+            $model->slug = LibraryPdf::where('slug', '!=', $model->slug)->where('slug', $slug)->exists() ? $slug.'-'.uniqid() : $slug;
+        });
+    }
+}

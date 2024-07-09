c14c755454310336a70a697445aff30d4b0d3c35
vadimprokofev14 <vadim61prokofev19971401@gmail.com>
2024-07-09 14:52:44 +0500

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Миссия компании</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <!-- <link rel="stylesheet" href="/js/dflip/css/dflip.min.css">
-    <link rel="stylesheet" href="/js/dflip/css/themify-icons.min.css"> -->
+    <link rel="stylesheet" href="/js/dflip/css/dflip.min.css">
+    <link rel="stylesheet" href="/js/dflip/css/themify-icons.min.css">
     <link rel="stylesheet" href="{{asset("css/jquery.fancybox.css")}}">
     <link rel="stylesheet" href="{{asset("css/main.css")}}">
     <link rel="stylesheet" href="{{asset("css/styles.css")}}?v=1.37">

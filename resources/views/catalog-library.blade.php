577635c2def6d58c02f764cdf67634bc35fee79e
Huldul <kiriunchik@gmail.com>
2024-07-09 15:34:54 +0500

    Catalog online

--- resources/views/catalog-library.blade.php
+++ resources/views/catalog-library.blade.php
@@ -18,7 +18,7 @@
                     <path d="M11.0037 8L6.00372 13L6.00372 3L11.0037 8Z" fill="#6EB513" />
                 </svg>
             </li>
-            <li>Каталог</li>
+            <li>Библиотека каталогов</li>
 
         </ul>
     </div>
@@ -32,6 +32,7 @@
             @foreach ($inner_types as $inner_type)
                 <a href="{{ route('catalog-online', ['locale' => app()->getLocale(), 'slug' => $inner_type->slug]) }}" class="catalog-library__card" data-src="#report_1">
                     <img src="{{ asset('storage/' . $inner_type->image) }}" alt="{{ $inner_type->title }}">
+                    <span>{{ $inner_type->title }}</span>
                 </a>
             @endforeach
         </div>

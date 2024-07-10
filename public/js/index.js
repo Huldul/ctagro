e2f81dc33be4ddecbc1b712c0c84e39a4f7dec2c
vadimprokofev14 <vadim61prokofev19971401@gmail.com>
2024-07-10 15:38:55 +0500

    upd css/js

--- public/js/index.js
+++ public/js/index.js
@@ -252,6 +252,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const btn = container.querySelector('.products-inner__btn');
         const leftBlock = container.querySelector('.service__left');
 
+        if (moreBtn && moreText) {
             moreBtn.addEventListener('click', () => {
                 if (!moreText.classList.contains('active')) {
                     moreText.classList.add('active');
@@ -269,6 +270,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     }
                 }
             });
+        }
     });
 
     // Получаем ссылки на все табы

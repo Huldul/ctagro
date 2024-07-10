a59f6b67628c7d344e1a35ec89b05cd72c82b5e2
Huldul <kiriunchik@gmail.com>
2024-07-10 15:21:59 +0500

    Product edit

--- resources/views/news-inner.blade.php
+++ resources/views/news-inner.blade.php
@@ -60,7 +60,7 @@
     @php
     $images = json_decode($news->mult_images, true);
     @endphp
-
+    @dd($images)
     @if ($news->mult_images != "")
     <section class="gallery container indent">
         <h2 class="gallery__title title">Галерея</h2>

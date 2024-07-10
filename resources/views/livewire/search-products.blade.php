2995b9ec7d175473ab8e905ffa46d2198e8e5d6d
Huldul <kiriunchik@gmail.com>
2024-07-10 19:10:31 +0500

    liwewire search

--- resources/views/livewire/search-products.blade.php
+++ resources/views/livewire/search-products.blade.php
@@ -1,13 +1,15 @@
-<form wire:submit.prevent="updatedSearchTerm">
-    <input type="search" wire:model.debounce.300ms="searchTerm" required>
-    <button type="submit">найти</button>
-</form>
-@if ($results)
-    <div class="search__result">
-        @foreach ($results as $result)
-            <div>{{ $result->title }}</div>
-        @endforeach
-    </div>
-@else
-    <div class="search__result-not-found">поиск не дал результатов</div>
-@endif
+<div>
+    <form wire:submit.prevent="updatedSearchTerm">
+        <input type="search" wire:model.debounce.300ms="searchTerm" required>
+        <button type="submit">найти</button>
+    </form>
+    @if ($results)
+        <div class="search__result">
+            @foreach ($results as $result)
+                <div>{{ $result->title }}</div>
+            @endforeach
+        </div>
+    @else
+        <div class="search__result-not-found">поиск не дал результатов</div>
+    @endif
+</div>

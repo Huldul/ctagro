aa5e31460fff754d44cca4e90465fc6a819f7772
Huldul <kiriunchik@gmail.com>
2024-07-11 15:28:07 +0500

    Extra blocks

--- app/Http/Controllers/ProductSearchController.php
+++ app/Http/Controllers/ProductSearchController.php
@@ -4,13 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Type;
+use App\Models\Subtype;
 
 class ProductSearchController extends Controller
 {
     public function search(Request $request)
     {
         $query = $request->input('query');
-        $products = Product::where('title', 'LIKE', "%{$query}%")->get();
+
+        // Поиск по продуктам
+        $products = Product::where('title', 'LIKE', "%{$query}%")
+            ->orWhereHas('type', function($q) use ($query) {
+                $q->where('name', 'LIKE', "%{$query}%");
+            })
+            ->orWhereHas('subtype', function($q) use ($query) {
+                $q->where('name', 'LIKE', "%{$query}%");
+            })
+            ->get();
 
         return response()->json($products);
     }

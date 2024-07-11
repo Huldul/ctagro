7734b3ea6b08e1c4ed3d08fbdbefe49bc93a3d05
Huldul <kiriunchik@gmail.com>
2024-07-11 15:29:25 +0500

    Extra blocks

--- app/Http/Controllers/ProductSearchController.php
+++ app/Http/Controllers/ProductSearchController.php
@@ -12,7 +12,16 @@ class ProductSearchController extends Controller
     public function search(Request $request)
     {
         $query = $request->input('query');
-        $products = Product::where('title', 'LIKE', "%{$query}%")->get();
+
+        // Поиск по продуктам
+        $products = Product::where('title', 'LIKE', "%{$query}%")
+            ->orWhereHas('type', function($q) use ($query) {
+                $q->where('name', 'LIKE', "%{$query}%");
+            })
+            ->orWhereHas('subtype', function($q) use ($query) {
+                $q->where('name', 'LIKE', "%{$query}%");
+            })
+            ->get();
 
         return response()->json($products);
     }

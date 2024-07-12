0b2939a6e53e3140af3761c3118ea332c63be90e
Huldul <kiriunchik@gmail.com>
2024-07-12 16:44:59 +0500

    Video to prods

--- resources/views/products-inner.blade.php
+++ resources/views/products-inner.blade.php
@@ -162,83 +162,86 @@
                     </div>
                 </div>
             </section>
-            <section class="products-inner__video container indent">
-                <h2 class="products-inner__title title">Хотите узнать о наc больше? <br><span>Переходите на наш youtube
-                        канал.</span></h2>
-                                <div class="swiper video__slider">
-                    <div class="swiper-wrapper">
-                        @foreach ($product->videos as $video)
-                            <?php
-                            $thumbnail_url = './img/about-img.png'; // URL изображения по умолчанию
 
-                            // Проверка для YouTube с форматом /embed/
-                            if (preg_match('/youtube\.com\/embed\/([^\\?\\&]+)/', $video->url, $matches)) {
-                                if (isset($matches[1])) {
-                                    $video_id = $matches[1];
-                                    $thumbnail_url = "https://img.youtube.com/vi/{$video_id}/hqdefault.jpg";
-                                }
-                            }
-                            // Проверка для Vimeo
-                            elseif (preg_match('/vimeo\.com\/(\d+)/', $video->url, $matches)) {
-                                if (isset($matches[1])) {
-                                    $video_id = $matches[1];
-                                    $vimeo_api_url = "https://vimeo.com/api/v2/video/{$video_id}.json";
-                                    $vimeo_response = json_decode(file_get_contents($vimeo_api_url), true);
-                                    if (isset($vimeo_response[0]['thumbnail_large'])) {
-                                        $thumbnail_url = $vimeo_response[0]['thumbnail_large'];
+            <section class="products-inner__video container indent">
+                @if (!empty($product->videos))
+                    <h2 class="products-inner__title title">Хотите узнать о наc больше? <br><span>Переходите на наш youtube
+                            канал.</span></h2>
+                                    <div class="swiper video__slider">
+                        <div class="swiper-wrapper">
+                            @foreach ($product->videos as $video)
+                                <?php
+                                $thumbnail_url = './img/about-img.png'; // URL изображения по умолчанию
+
+                                // Проверка для YouTube с форматом /embed/
+                                if (preg_match('/youtube\.com\/embed\/([^\\?\\&]+)/', $video->url, $matches)) {
+                                    if (isset($matches[1])) {
+                                        $video_id = $matches[1];
+                                        $thumbnail_url = "https://img.youtube.com/vi/{$video_id}/hqdefault.jpg";
                                     }
                                 }
-                            }
-                            ?>
-                            <div class="swiper-slide video__slider-slide">
+                                // Проверка для Vimeo
+                                elseif (preg_match('/vimeo\.com\/(\d+)/', $video->url, $matches)) {
+                                    if (isset($matches[1])) {
+                                        $video_id = $matches[1];
+                                        $vimeo_api_url = "https://vimeo.com/api/v2/video/{$video_id}.json";
+                                        $vimeo_response = json_decode(file_get_contents($vimeo_api_url), true);
+                                        if (isset($vimeo_response[0]['thumbnail_large'])) {
+                                            $thumbnail_url = $vimeo_response[0]['thumbnail_large'];
+                                        }
+                                    }
+                                }
+                                ?>
+                                <div class="swiper-slide video__slider-slide">
+                                    <div class="video__slide">
+                                        <div data-id="" data-url="{{$video->url}}" class="video__slide-video">
+                                            <img src="{{ asset("storage/".$video->image) }}" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+
+
+                            {{-- <div class="swiper-slide video__slider-slide">
                                 <div class="video__slide">
-                                    <div data-id="" data-url="{{$video->url}}" class="video__slide-video">
-                                        <img src="{{ asset("storage/".$video->image) }}" alt="">
+                                    <div class="video__slide-video">
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/aWjtf9VWYUM?si=LinI_HE9-qmZu_YS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                                     </div>
                                 </div>
                             </div>
-                        @endforeach
-
-
-                        {{-- <div class="swiper-slide video__slider-slide">
-                            <div class="video__slide">
-                                <div class="video__slide-video">
-                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/aWjtf9VWYUM?si=LinI_HE9-qmZu_YS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                            <div class="swiper-slide video__slider-slide">
+                                <div class="video__slide">
+                                    <div class="video__slide-video">
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/mjMVWQEGrOQ?si=isrJ1bLmfUBYmSoY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="swiper-slide video__slider-slide">
-                            <div class="video__slide">
-                                <div class="video__slide-video">
-                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/mjMVWQEGrOQ?si=isrJ1bLmfUBYmSoY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                            <div class="swiper-slide video__slider-slide">
+                                <div class="video__slide">
+                                    <div class="video__slide-video">
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/aWjtf9VWYUM?si=LinI_HE9-qmZu_YS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                    </div>
                                 </div>
-                            </div>
+                            </div> --}}
                         </div>
-                        <div class="swiper-slide video__slider-slide">
-                            <div class="video__slide">
-                                <div class="video__slide-video">
-                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/aWjtf9VWYUM?si=LinI_HE9-qmZu_YS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                                </div>
+                        <div class="nav-btn">
+                            <div class="swiper-button-prev">
+                                <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <rect x="56" y="56" width="56" height="56" rx="28" transform="rotate(-180 56 56)" fill="#dadada" />
+                                    <path d="M31 34.24L24 27.52L31 20.8" stroke="#636362" stroke-width="3"/>
+                                    </svg>
+
                             </div>
-                        </div> --}}
-                    </div>
-                    <div class="nav-btn">
-                        <div class="swiper-button-prev">
-                            <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <rect x="56" y="56" width="56" height="56" rx="28" transform="rotate(-180 56 56)" fill="#dadada" />
-                                <path d="M31 34.24L24 27.52L31 20.8" stroke="#636362" stroke-width="3"/>
-                                </svg>
+                            <div class="swiper-button-next">
+                                <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <rect width="56" height="56" rx="28" fill="#dadada" />
+                                    <path d="M25 21.76L32 28.48L25 35.2" stroke="#636362" stroke-width="3"/>
+                                    </svg>
 
-                        </div>
-                        <div class="swiper-button-next">
-                            <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <rect width="56" height="56" rx="28" fill="#dadada" />
-                                <path d="M25 21.76L32 28.48L25 35.2" stroke="#636362" stroke-width="3"/>
-                                </svg>
-
+                            </div>
                         </div>
                     </div>
-                </div>
+                @endif
                 <div class="products-inner__video-wrapper">
                     @foreach ($product->news as $item)
                         <div class="products-inner__card">

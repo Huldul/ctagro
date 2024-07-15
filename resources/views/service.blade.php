d1c1a7d582d9dc4cd543e6ea842add6efb8b3490
Huldul <kiriunchik@gmail.com>
2024-07-15 11:03:26 +0500

    hidden on service

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -28,7 +28,7 @@
                     <div class="service__left-text">
                         <h2 class="title">{!! $page->subtitle1 !!}</h2>
                         {!! $page->main1 !!}
-                        <!-- <div class="hidden-text">{!! $page->desc1 !!}</div> -->
+                        <div class="hidden-text">{!! $page->desc1 !!}</div>
                         </div>
                         <button class="more-btn">Подробнее</button>
                     </div>
@@ -60,7 +60,7 @@
                     <div class="service__left-text">
                         <h2 class="title">{!! $page->subtitle2 !!}</h2>
                         {!! $page->main2 !!}
-                        <!-- <div class="hidden-text">{!! $page->desc2 !!}</div> -->
+                        <div class="hidden-text">{!! $page->desc2 !!}</div>
                         </div>
                         <button class="more-btn">Подробнее</button>
                     </div>

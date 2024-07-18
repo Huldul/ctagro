92010fc4493b545b3b63e04e8ce39af778621d61
Huldul <kiriunchik@gmail.com>
2024-07-18 15:31:57 +0500

    extra blocks

--- resources/views/products-inner.blade.php
+++ resources/views/products-inner.blade.php
@@ -107,27 +107,42 @@
                                 @endif
                             </div>
                         </div>
-                        @dd($product->blocks)
+
                         @foreach($product->blocks as $index => $block)
                             @if($index % 2 == 0)
-                            <div class="products-inner__container about__container">
-                                <div class="products-inner__right">
-                                    <h2>{{ $block->title }}</h2>
-                                    <p>{!! $block->main !!}</p>
-                                    <p class="hidden-text">{{ $block->hidden_main }}</p>
+                                <div class="products-inner__container about__container">
+                                    <div class="products-inner__right">
+                                        <h2>{{ $block->title }}</h2>
+                                        <p>{!! $block->main !!}</p>
+                                        <p class="hidden-text">{{ $block->hidden_main }}</p>
 
-                                    @if($block->hidden_main != "")
-                                        <button class="more-btn products-inner__btn">
-                                            Подробнее
-                                        </button>
-                                    @endif
+                                        @if($block->hidden_main != "")
+                                            <button class="more-btn products-inner__btn">
+                                                Подробнее
+                                            </button>
+                                        @endif
+                                    </div>
+                                    <div class="products-inner__left">
+                                        <img src="{{ asset('storage/' . $block->image) }}" alt="">
+                                    </div>
                                 </div>
-                                <div class="products-inner__left">
-                                    <img src="{{ asset('storage/' . $block->image) }}" alt="">
+                            @else
+                                <div class="products-inner__container about__container products-inner-reverse">
+                                    <div class="products-inner__left">
+                                        <img src="{{ asset('storage/' . $block->image) }}" alt="">
+                                    </div>
+                                    <div class="products-inner__right">
+                                        <h2>{{ $block->title }}</h2>
+                                        <p>{!! $block->main !!}</p>
+                                        <p class="hidden-text">{{ $block->hidden_main }}</p>
+
+                                        @if($block->hidden_main != "")
+                                            <button class="more-btn products-inner__btn">
+                                                Подробнее
+                                            </button>
+                                        @endif
+                                    </div>
                                 </div>
-                            </div>
-
-
                             @endif
                         @endforeach
 

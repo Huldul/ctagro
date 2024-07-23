4e3784434adfa5dcdf2c8ddadea4948325bea770
Huldul <kiriunchik@gmail.com>
2024-07-23 16:23:25 +0500

    other extra blocks

--- routes/web.php
+++ routes/web.php
@@ -88,7 +88,7 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('/catalog-online/{slug}', function ($slug) {
         return app()->call('App\Http\Controllers\PageController@catalog_online', ['locale' => 'ru', 'slug' => $slug]);
     })->name('catalog-online.without_locale');
-    Route::get('/library-online', function ($slug) {
+    Route::get('/library-online', function () {
         return app()->call('App\Http\Controllers\PageController@library_online', ['locale' => 'ru']);
     })->name('library_online.without_locale');
     Route::get('/catalog-brand', function () {

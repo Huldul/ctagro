1357737964d8e6e10f8e4dc7f5a5a6b9d648861e
vadimprokofev14 <vadim61prokofev19971401@gmail.com>
2024-08-01 10:28:21 +0500

    upd css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,7 +24,7 @@
     <link rel="stylesheet" href="/js/dflip/css/themify-icons.min.css">
     <link rel="stylesheet" href="{{asset("css/jquery.fancybox.css")}}">
     <link rel="stylesheet" href="{{asset("css/main.css")}}">
-    <link rel="stylesheet" href="{{asset("css/styles.css")}}?v=1.88">
+    <link rel="stylesheet" href="{{asset("css/styles.css")}}?v=1.89">
     @livewireStyles
 </head>
 

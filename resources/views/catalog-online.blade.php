7ae8efa7e8a38a49e9fa83b28b5a2b485c7fccc7
Huldul <90949230+Huldul@users.noreply.github.com>
2024-08-19 11:37:39 +0500

    breadcumps

--- resources/views/catalog-online.blade.php
+++ resources/views/catalog-online.blade.php
@@ -1,6 +1,25 @@
 @extends('layout')
 @section('content')
 <main>
+    <div class="breadcrumbs container">
+        <ul>
+            <li>
+                <a href="/">Главная</a>
+            </li>
+            <li>
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11.0037 8L6.00372 13L6.00372 3L11.0037 8Z" fill="#6EB513" />
+                </svg>
+            </li>
+            <li><a href="{{ route('catalog-library', ['locale' => app()->getLocale()]) }}">@trans('catalog_library')</a></li>
+            <li>
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11.0037 8L6.00372 13L6.00372 3L11.0037 8Z" fill="#6EB513" />
+                </svg>
+            </li>
+            <li>{{ $inner_type->title }}</li>
+        </ul>
+    </div>
 <section class="first__block">
         <img src="{{ asset('img/first-block-img.png') }}" alt="">
         <div class="first__block-wrapper container">

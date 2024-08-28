ba8a45581a0247a87faae3fec0a62b3f632a1651
vadimprokofev14 <vadim61prokofev19971401@gmail.com>
2024-08-28 11:13:40 +0500

    upd policy

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -3,7 +3,7 @@
 @section('content')
         <main>
             <section class="first__block">
-                <img src="{{asset("img/first-block-img.png")}}" alt="">
+                <img src="{{asset("img/politika_oblojka.jpeg")}}" alt="">
                 <div class="first__block-wrapper container">
                     <h1>Политика конфиденциальности</h1>
                 </div>

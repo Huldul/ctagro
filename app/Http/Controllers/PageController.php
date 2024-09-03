ba37eb2532f0b5c473710ccaabce0196319e2801
Huldul <90949230+Huldul@users.noreply.github.com>
2024-09-03 14:39:38 +0500

    1215

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -20,7 +20,7 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function index(){
-        $untrMeds = News::paginate(5);
+        $untrMeds = News::orderBy('created_at', 'desc')->paginate(5);
         $meds = $this->translateCollection($untrMeds ,app()->getLocale());
         $untrpage = CustomPage::findOrFail(1);
         $page = $untrpage->translate(app()->getLocale());
